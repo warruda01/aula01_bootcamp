cfe579258252a86170ad8854ae3ae9e52e18184e
warruda01 <warruda01@gmail.com>
2025-08-31 18:19:03 -0300

    Alteração exercicio 1

--- Exercicio01.py
+++ Exercicio01.py
@@ -2,7 +2,7 @@ nome = input("digite seu nome -->")
 print (nome)
 print(len(nome))
 
-
+print("Olá, " + input("Digite seu nome: ") + "!")
 
 #O len pode ser usado para contagem de caracteres em senhas por exemplo
 #senha = input("digite sua senha com oito digitos-->")
